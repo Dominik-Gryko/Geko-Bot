2b5cf5bd1312668983af8e29dc67d473044bcf7b
dominik <domgryko@gmail.com>
2023-08-31 01:01:36 +0100

    updated fun.py

--- cogs/fun.py
+++ cogs/fun.py
@@ -33,31 +33,6 @@ class fun(commands.Cog):
         link = "https://www.youtube.com/"
         await ctx.send(link)
         await print(f"{ctx.author.name} requested youtube link.")
-
-    @commands.command(name = "tripaloski", aliases = ["tri", "paloski"])
-    async def tripaloski(self, ctx):
-        link = "https://www.youtube.com/watch?v=x7W8Y_g3W1s&ab_channel=jukkeboy"
-        await ctx.send(link)
-        await print(f"{ctx.author.name} requested tripaloski link.")
-    
-    @commands.command(name = "nanofly")
-    async def nanofly(self, ctx):
-        link = "https://nanoflystore.com/"
-        await ctx.send(link)
-        await print(f"{ctx.author.name} requested nanoflystore.")
-
-    @commands.command(name = "johncena")
-    async def johncena(self, ctx):
-        cena = """
-Imagine John Cena, the professional wrestler, 
-sitting atop a giant hot dog. He's straddling the hot dog as if it were a horse. 
-John Cena's iconic muscular physique contrasts with the whimsical nature of the scene. 
-He has a determined and confident expression on his face as he "rides" the hot dog with his hands gripping its bun, almost as if he's in the middle of a wrestling match. 
-The hot dog itself is larger than life, with a bright yellow mustard streak along its length and a line of ketchup as if it were a racing stripe. 
-The whole scene is a playful and humorous portrayal, capturing the larger-than-life personality of John Cena and the imaginative concept of him riding a hot dog.
-"""
-
-        await ctx.send(cena + "https://www.cultofwhatever.com/wp-content/uploads/2016/02/gif-john-cena-eyebrows.gif")
         
 async def setup(bot):
     await bot.add_cog(fun(bot))
